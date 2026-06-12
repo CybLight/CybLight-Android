7f5a6c900882871ba02be1a5d053aeb318a01264
ⒹⒶⓃⒾⒺⓁ ⓀⓄⒷⓏⒾⓈⓉⓎⒾ <danyil.kobzystyi@gmail.com>
2026-06-13 00:07:24 +0300

    Релиз v0.4.8: реакции в чате, галочки прочитанных, Postmaster и биометрия.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "org.cyblight.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "0.4.7"
+        versionCode = 20
+        versionName = "0.4.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
